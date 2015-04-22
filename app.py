80a9a28e637caa175a75c87d15b0535ec1409853
Alec Potluri <brookdalealec@gmail.com>
2015-04-22 14:55:31 -0500

    can add, remove, initially load VALID elements

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, render_template, redirect
 from flask.ext.basicauth import BasicAuth
 import json
 import sys
-import pprint
+import string
 
 if len( sys.argv) != 3:
 	print( 'usage: ' + sys.argv[0] + ' secrets_file site_name')
@@ -25,7 +25,16 @@ except:
 	print( 'Could not parse ' + sys.argv[1] + '. Exiting')
 	exit()
 
+if sys.argv[2] == 'lib' or sys.argv[2] == 'static' or sys.argv[2] == 'templates':
+	print( 'Site name is invalid. Exiting')
+	exit()
+
 site = Site( sys.argv[2])
+for char in site.name:
+	if char not in string.ascii_letters + string.digits + '_':
+		print( 'Site name is invalid. Exiting')
+		exit()
+site.name += '/'
 
 app = Flask(__name__)
 
@@ -36,7 +45,7 @@ basic_auth = BasicAuth( app)
 
 @app.route( '/')
 def route_index():
-	if site.name + '/index' in site.render().keys():
+	if site.name + 'index' in site.render().keys():
 		return 'well there\'s an index page...'
 	else:
 		return 'please add an index page!!'
@@ -58,6 +67,8 @@ def edit_page():
 @app.route( '/edit/goto/', methods=['POST'])
 @basic_auth.required
 def edit_page_goto():
+	if request.form['to_edit'] == 'Choose a page to edit...':
+		return redirect( '/edit/')
 	return redirect( '/edit/' + request.form['to_edit'])
 
 @app.route( '/edit/add/', methods=['POST'])
@@ -70,13 +81,38 @@ def edit_page_add():
 @app.route( '/edit/<path:page_name>', methods=['POST', 'GET'])
 @basic_auth.required
 def edit_page_specific( page_name):
-	return render_template( 'edit_specific.html', page_name = page_name)
+	if page_name not in site.render().keys():
+		return redirect( '/edit')
+	ele_add = ElementAddForm( request.form)
+	ele_add.to_add.data = ''
+	ele_remove = ElementRemoveForm( request.form)
+	ele_remove.choose( site.render()[page_name].render())
+	return render_template( 'edit_specific.html', ele_remove = ele_remove, ele_add = ele_add, page_name = page_name)
 
-@app.route( '/edit/<path:page_name>/delete/', methods=['POST', 'GET'])
+@app.route( '/edit/<path:page_name>/delete/', methods=['POST'])
 @basic_auth.required
 def edit_page_specific_delete( page_name):
 	site.remove( page_name)
 	return redirect( '/edit/')
+
+@app.route( '/edit/<path:page_name>/add/', methods=['POST'])
+@basic_auth.required
+def edit_page_specific_add( page_name):
+	ele_to_add = Element( 'text')
+	if not request.form['to_add']:
+		return redirect( '/edit/' + page_name)
+	ele_to_add.content = request.form['to_add']
+	ele_to_add.location = 'body'
+	site.render()[page_name].add( ele_to_add)
+	return redirect( '/edit/' + page_name)
+
+@app.route( '/edit/<path:page_name>/remove/', methods=['POST'])
+@basic_auth.required
+def edit_page_specific_remove( page_name):
+	if request.form['to_remove'] == '***Choose an element to remove***':
+		return redirect( '/edit/' + page_name)
+	site.render()[page_name].remove( request.form['to_remove'])
+	return redirect( '/edit/' + page_name)
 
 if __name__ == '__main__':
 	app.run( host = '0.0.0.0', debug = True)
--- lib/Forms.py
+++ lib/Forms.py
@@ -1,4 +1,5 @@
-from wtforms import Form, TextField, SelectField
+from wtforms import Form, TextField, SelectField, TextAreaField
+from lib.Element import *
 import pprint
 
 class PageAddForm( Form):
@@ -6,12 +7,38 @@ class PageAddForm( Form):
 
 def listToPair( list):
 	to_ret = []
-	for ele in list:
-		to_ret += [ ( ele, ele)]
+	for page in list:
+		to_ret += [ ( page, page)]
 	return to_ret
+
+def checkElementContentLength( content):
+	if len( content) > 25:
+		return '...'
+	return ''
+
+def renderElementList( list):
+	to_ret = []
+	for ele in list.keys():
+		tmp_ele = Element( 'text')
+		tmp_ele.load( list[ele])
+		to_ret += [ ( tmp_ele.id, '"' + tmp_ele.content[:25] + checkElementContentLength( tmp_ele.content) + '"')]
+	return to_ret
+
 
 class PageEditForm( Form):
 	to_edit = SelectField( 'Page Name')
 	
 	def choose( self, pages):
 		self.to_edit.choices = listToPair( pages)
+		self.to_edit.choices.insert( 0, ( 'Choose a page to edit...', 'Choose a page to edit...'))
+
+class ElementAddForm( Form):
+	to_add = TextAreaField( 'Element Content')
+
+class ElementRemoveForm( Form):
+	to_remove = SelectField( 'Element Name')
+	
+	def choose( self, elements):
+		self.to_remove.choices = renderElementList( elements)
+	#	self.to_remove.choices = []
+		self.to_remove.choices.insert( 0, ( '', '***Choose an element to remove***'))
